a27e59e7970e6a52da36cd530f66ca399a225347
Mohammad Tanvir <tanvir0102@gmail.com>
2022-11-26 13:56:02 +0530

    working vnetwork module

--- vnetwork/main.tf
+++ vnetwork/main.tf
@@ -8,14 +8,14 @@ locals {
     environment = local.environment
   }
 } 
-# Azure Resource Group
+# Resource-1: Azure Resource Group
 resource "azurerm_resource_group" "rg" {
   name = "${local.resource_name_prefix}-${var.resource_group_name}"
   location = var.resource_group_location
   tags = local.common_tags
 }
 
-# Create Virtual Network
+# Resource-1: Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "${local.resource_name_prefix}-${var.vnet_name}"
   address_space       = var.vnet_address_space
